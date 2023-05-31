243b814c9462cdba132f5a081171f1ff4ae11eb0
Amirzhan <albert.git1@gmail.com>
2023-06-01 04:30:25 +0500

    заготовка

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:18.16-alpine as build
+WORKDIR /srv
+COPY . .
+RUN npm ci && npm run build && npm ci --omit=dev
+
+FROM node:16-alpine
+WORKDIR /srv
+COPY --from=build /srv/node_modules ./node_modules
+COPY --from=build /srv/dist ./dist
+CMD node dist/main.js
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASS'),
+        database: configService.get('DB_NAME'),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
